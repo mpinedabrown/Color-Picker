67bc3a0a70814bb5842d22145cd166b63e7a2c84
Melissa Pineda Brown <mpinedabrown@gmail.com>
2026-09-11 21:09:33 -0600

    commit

--- script.js
+++ script.js
@@ -13,26 +13,17 @@ function initColorPicker() {
     setColorPickerEventListener(colorBox, rgb);
 }
 
-function setColorPickerEventListener (colorBox, rgb, colorPickers) {
-
-    for (let i= 0; i < pickerElements.length; i++) {
+function setColorPickerEventListener (colorBox, rgb, pickerElements) {
+    let pickerLen = pickerElements.length;
+    for (let i= 0; i < pickerLen; i++) {
         pickerElements[i].addEventListener('change', () => {
-            setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+            console.log("Red value:", rgb.red.value);
+            let red=rgb.red.value;
+            let green = rgb.green.value;
+            let blue = rgb.blue.value;
+            setBoxBGColor(colorBox, red, green, blue);
         });
     }
-
-   /* rgb.red.addEventListener('change', () => {
-        console.log("Red value:", rgb.red.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-    });
-    rgb.green.addEventListener('change', () => {
-        console.log("Green value:", rgb.green.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-    });
-    rgb.blue.addEventListener('change', () => {
-        console.log("Blue value:", rgb.blue.value);
-        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-    });*/
 }
 
 function setBoxBGColor (colorBox, red, green, blue) {
