7b81438880c34764a9159aac10908475ec0a301b
Melissa Pineda Brown <mpinedabrown@gmail.com>
2026-09-11 19:49:13 -0600

    testing commit

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function initColorPicker() {
 function setColorPickerEventListener (colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
         console.log("Red value:", rgb.red.value);
+        setBoxBGColor(colorBox, 150, 150, 150);
     });
     rgb.green.addEventListener('change', () => {
         console.log("Green value:", rgb.green.value);
@@ -22,4 +23,9 @@ function setColorPickerEventListener (colorBox, rgb) {
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value:", rgb.blue.value);
     });
+}
+
+function setBoxBGColor (colorBox, red, green, blue) {
+    rgbVal = [red, green, blue].join(',');
+    colorBox.style.backgroundColor = "rgb("+ rgbVal +")";
 }
