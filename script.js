abf8f953351656becf73f390c142feaafae393b0
Melissa Pineda Brown <mpinedabrown@gmail.com>
2026-09-11 20:47:03 -0600

    commit

--- script.js
+++ script.js
@@ -9,20 +9,30 @@ function initColorPicker() {
         green: document.getElementById("green"),
         blue: document.getElementById("blue"),
     };
+    let colorPickers = document.getElementsByClassName("picker");
     setColorPickerEventListener(colorBox, rgb);
 }
 
-function setColorPickerEventListener (colorBox, rgb) {
-    rgb.red.addEventListener('change', () => {
+function setColorPickerEventListener (colorBox, rgb, colorPickers) {
+
+    for (let i= 0; i < pickerElements.length; i++) {
+        pickerElements[i].addEventListener('change', () => {
+            setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+        });
+    }
+
+   /* rgb.red.addEventListener('change', () => {
         console.log("Red value:", rgb.red.value);
-        setBoxBGColor(colorBox, 150, 150, 150);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.green.addEventListener('change', () => {
         console.log("Green value:", rgb.green.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value:", rgb.blue.value);
-    });
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+    });*/
 }
 
 function setBoxBGColor (colorBox, red, green, blue) {
